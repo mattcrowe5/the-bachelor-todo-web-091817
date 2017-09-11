8ecef4e3069137f06d79a5449c0c26e8ef436d15
Matt Crowe <crowe.matthew5@gmail.com>
2017-09-11 19:22:30 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,49 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |contestant_hash|
+    if contestant_hash["status"] == "Winner"
+      return contestant_hash["name"].split.first
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, contestant|
+    data[season].each do |contestant_hash|
+      if contestant_hash["occupation"] == occupation
+        return contestant_hash["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  hometown_count = 0
+  data.each do |season, contestant|
+    data[season].each do |contestant_hash|
+      if contestant_hash["hometown"] == hometown
+        hometown_count = hometown_count + 1
+      end
+    end
+  end
+  hometown_count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, contestant|
+    data[season].each do |contestant_hash|
+      if contestant_hash["hometown"] == hometown
+        return contestant_hash["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  total_age = 0
+  data[season].each do |contestant_hash|
+    total_age += contestant_hash["age"].to_f
+  end
+  (total_age/data[season].count).round
 end
